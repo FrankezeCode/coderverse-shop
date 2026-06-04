7da3dee83d431b5af6dda1ef47a131be608daf39
FrankezeCode <Franklynezeugonna@gmail.com>
2026-06-04 10:19:33 +0100

    favicon update

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,8 +28,14 @@ export const metadata: Metadata = {
   title: "CODERVERSE SHOP",
   description:
     "Shop for the best products from the best brands in the world.",
+  manifest: "/site.webmanifest",
   icons: {
-    icon: "/favicon.ico",
+    icon: [
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    ],
+    apple: "/apple-touch-icon.png",
+    shortcut: "/favicon.ico",
   },
 };
 
